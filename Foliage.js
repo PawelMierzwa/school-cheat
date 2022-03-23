ecd3959040a2812049e7b6515d86df4c5bfc0ef5
Paweł Mierzwa <cxnshx@gmail.com>
2022-03-23 19:00:36 +0100

    Dots

--- Foliage.js
+++ Foliage.js
@@ -1,5 +1,19 @@
+
 function siecing() {
     
-
-
+//    wynik.innerHTML =;
 }
+
+
+function spacjowanie() {
+    let kropka= adres.value;
+    kropka = kropka.replaceAll(" ",".");
+    adres.value = kropka;
+}
+// function zamiana(){
+//     let tekst=pole.value;
+//     const szukana=prompt("Podaj szukany wyraz");
+//     const zmiana=prompt("Podaj na co zamienaimy");
+//     tekst=tekst.replaceAll(szukana,zmiana);
+//     pole.value=tekst;
+//     }
