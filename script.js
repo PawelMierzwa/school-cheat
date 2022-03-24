345e9080cdc4463b46638644cf9b864018ada091
Paweł Mierzwa <cxnshx@gmail.com>
2022-03-24 23:06:39 +0100

    Spacing WiP 24.03.2022 23:06
    Co-Authored-By: Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,22 @@
+// let count3 = 0;
+
+// function spacjowanie() {
+//     if(count3==1){
+//         return;
+//     }
+//     let dot = adres.value;
+//     dot = dot.replaceAll(" ",".");
+//     adres.value = dot;
+
+//     licznik.innerHTML = count3;
+// }
+
+// let count2 = 0;
+// let count = 1;
+
+// function kropkowanie(){
+//     let text = adres.value;
+//     let x = text[count2-1];
+//     count2 = text.length;
+
+//     count3++;
