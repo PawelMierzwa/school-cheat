e017273b14cea07db3255e17e8db0391248a56f1
Paweł Mierzwa <cxnshx@gmail.com>
2022-03-25 20:41:24 +0100

    Refactors
    Co-Authored-By: DmytroSobol <79514945+DmytroSobol@users.noreply.github.com>

--- template/header.php
+++ template/header.php
@@ -4,52 +4,46 @@
 </head>
 <body>
 
+    <div class="menu">
     <?php
         $file = $_SERVER['REQUEST_URI'];
 
         switch($file){
             case '/foliage':
                 echo '
-                    <div class="menu">
                     <a href="/">Home</a>
                     <a class="active" href="/foliage">Foliarz</a>
                     <a href="/matma">Matma</a>
                     <a href="/about">About</a>
-                    </div>
                     ';
                 break;
             case '/matma':
                 echo '
-                    <div class="menu">
                     <a href="/">Home</a>
                     <a href="/foliage">Foliarz</a>
                     <a class="active" href="/matma">Matma</a>
                     <a href="/about">About</a>
-                    </div>
                     ';
                 break;
             case '/about':
                 echo '
-                    <div class="menu">
                     <a href="/">Home</a>
                     <a href="/foliage">Foliarz</a>
                     <a href="/matma">Matma</a>
                     <a class="active" href="/about">About</a>
-                    </div>
                     ';
                 break;
             default:
                 echo '
-                    <div class="menu">
                     <a class="active" href="/">Home</a>
                     <a href="/foliage">Foliarz</a>
                     <a href="/matma">Matma</a>
                     <a href="/about">About</a>
-                    </div>
                     ';
                 break;
         }
     ?>
+    </div>
 <!--<img onclick="" > -->
 <input class="dark" onclick="DarkTheme()" value="&#9789"></button>
 <script src="template/js/menu.js"></script>
