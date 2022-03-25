4342c475ca1e0acf24f934d924c11e15c136b0e8
Paweł Mierzwa <cxnshx@gmail.com>
2022-03-25 22:39:28 +0100

    dotting WiP8
    Co-Authored-By: DmytroSobol <79514945+DmytroSobol@users.noreply.github.com>

--- template/js/Foliage.js
+++ template/js/Foliage.js
@@ -8,47 +8,40 @@ let count2 = 0;
 let count = 1;
 
 
-// function kropkowanie(){
+ function kropkowanie(){
 
-    // //x = ostatnio wpisany znak :)
-
-
-    // let _kropkowac = kropkowac.value;
+    //x = ostatnio wpisany znak :)
     
-    // if (_kropkowac) {
-    //     let text = adres.value;
-    //     let x = text[count2-1];
-    //     let y = text[count2-2];
+        let text = adres.value;
+        let x = text[count2-1];
+        let y = text[count2-2];
 
-    //     if(x == " "){
-    //         if (y != ".") {
-    //             spacjowanie();
-    //         }
-    //         else{
-    //             text.value.slice(0, -1);
-    //         }
-    //     }
-    //     count2 = text.length;
+        if(x == " "){
+            if (y != ".") {
+                spacjowanie();
+            }
+            else{
+                text.pop();
+            }
+        }
 
-    //     if(count2 >= 14){
-    //         count=1;
-    //     }
-
-    //     if(x >= 0 && x <= 9){
-    //         if (count2 >= 14) return;
-    //         count++;
-    //     }
-
-    //     if (count == 3) {
-    //         if (count2 >= 14) return;
-    //         count = 0;
-    //         text = text + ".";
-    //         i = 0;
-    //     }
-        
-    //     adres.value = text;
-    // }
-// }
+        count2 = text.length;
+        if(count2 >= 14){
+            count=1;
+        }
+        if(x >= 0 && x <= 9){
+            if (count2 >= 14) return;
+            count++;
+        }
+        if (count == 3) {
+            if (count2 >= 14) return;
+            count = 0;
+            text = text + ".";
+            i = 0;
+        }
+     
+        adres.value = text;
+ }
 
 
 function Oblicz() {
