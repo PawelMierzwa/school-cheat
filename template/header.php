3b2055c049197682af7ad3285e4222e241f95104
DmytroSobol <79514945+DmytroSobol@users.noreply.github.com>
2022-03-30 20:02:30 +0200

    Fixed CSS

--- template/header.php
+++ template/header.php
@@ -1,7 +1,7 @@
 <html>
 
 <head>
-    <link rel="stylesheet" href="template/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="template/css/main.css" type="text/css">
 </head>
 
